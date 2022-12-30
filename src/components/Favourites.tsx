65055fee9993eb23ff3c9db0c7a3fa3cff52bb6f
nishat <nishatroyhana011@gmail.com>
2022-12-30 16:24:55 +0600

    my favourite page design

--- src/components/Favourites.tsx
+++ src/components/Favourites.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 
 const Favourites = () => {
+
+    type favItems = {
+        [key: string]: string,
+    };
+
+    const favSongs: favItems = JSON.parse(localStorage.getItem("my-fav") || "")
+
     return (
         <div>
-           <div className='bg-light py-5 '>
+            <div className='bg-light py-5 '>
                 <h2 className='text-center fs-1'>My Favourites</h2>
+            </div>
+            <div className='w-50 mx-auto'>
+                <ul className="list-group">
+                    {
+                        Object.keys(favSongs).map(k => <li className='list-group-item'> {favSongs[k]}</li>)
+                    }  
+                </ul>
+                
             </div>
         </div>
     );
--- src/components/MusicCard.tsx
+++ src/components/MusicCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 type musicProps = {
     
@@ -10,16 +10,35 @@ type musicProps = {
         title:string,
     }
 };
-
+type favItems = {
+    [key: string]: string,
+};
 function MusicCard(props: musicProps) {
+    const [fav,setFav]=useState<object>({});
+    const [str,setStr]=useState<string>("");
+
+    const handleFav = (id:number, name:string) =>{
+        
+        const strId = id.toString();
+        setStr(strId);
+        const item = localStorage.getItem('my-fav');
+        let favItems:favItems= {};
+        if(item){
+           favItems = JSON.parse(item)
+        }
+        strId in favItems? delete favItems[strId] : favItems[strId]=name;
+        setFav(favItems)
+        localStorage.setItem('my-fav', JSON.stringify(favItems));
+    }
+
     
     return (
-        <div className='col-sm-3 text-center pt-5 shadow-sm'>
+        <div className='col-6 col-md-4 text-center pt-5 shadow-sm'>
             <img src={props.song.album.cover} alt="" />
             <h5>{props.song.title}</h5>
-            <div>
-            
-            <i className="fa-regular fa-star"></i>
+            <div className=''>
+            <p>{props.song.id}</p>
+            <i className={`fa-star fs-3  ${str in fav? "fa-solid":"fa-regular"}`} onClick={()=>{handleFav(props.song.id,props.song.title)}}></i>
             </div>
         </div>
     );
